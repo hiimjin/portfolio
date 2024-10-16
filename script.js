4f7c4fff0e38720a0394e98a3c3015f32ddf7e32
Jin Lee <yjlpo2408@gmail.com>
2024-10-16 10:53:48 -0400

    script

---
+++ script.js
@@ -0,0 +1,13 @@
+const scroller = document.querySelector('.rf-cards-scroller-crop');
+const scrollLeftBtn = document.getElementById('scroll-left');
+const scrollRightBtn = document.getElementById('scroll-right');
+const cardElement = document.querySelector('.rf-cards-scroller-itemview');
+const cardStyle = getComputedStyle(cardElement);
+const cardWidth = cardElement.offsetWidth + parseInt(cardStyle.marginLeft) + parseInt(cardStyle.marginRight);
+
+scrollLeftBtn.addEventListener('click', () => {
+    scroller.scrollBy({ left: -cardWidth * 2.03, behavior: 'smooth' });
+});
+scrollRightBtn.addEventListener('click', () => {
+    scroller.scrollBy({ left: cardWidth * 2.03, behavior: 'smooth' });
+});
