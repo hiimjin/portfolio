730ccce25d62598651c277fb7b54fd06d578f61c
Jin Lee <yjlpo2408@gmail.com>
2024-11-11 21:33:36 -0500

    d

--- script.js
+++ script.js
@@ -35,7 +35,10 @@ const cardData = [
         // three
         title: " ",
         description: "Three is an undersaturated, low-stress social media platform where users express their thoughts on a daily prompt using three words. Three was my first project as iOS club developer.",
-        images: ["images/slides/three/three_1.png"]
+        images: [
+            "images/slides/three/three_1.png",
+            "images/slides/three/three_1.png"
+        ]
     },
     {
         title: " ",
